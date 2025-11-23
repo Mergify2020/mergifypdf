f26f7bff17902afe119ebcee7684ebb2f86201e1
Alan Morris <morrisalan2020@gmail.com>
2025-11-22 22:50:49 -0700

    update

--- src/app/api/projects/[id]/thumbnail/route.ts
+++ src/app/api/projects/[id]/thumbnail/route.ts
@@ -1,10 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 import { prisma } from "@/lib/prisma";
 
 export async function POST(
-  req: NextRequest,
+  req: Request,
   context: { params: { id: string } }
 ) {
   const { params } = context;
