b073bb6f215c2913c85e5346b0090fc6985b31f8
Alan Morris <morrisalan2020@gmail.com>
2025-11-25 00:52:23 -0700

    update

--- src/components/WorkspaceSettingsMenu.tsx
+++ src/components/WorkspaceSettingsMenu.tsx
@@ -1,17 +1,7 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import SettingsMenu from "./SettingsMenu";
 
-const ALLOWED_PREFIXES = ["/studio", "/", "/pricing"];
-
 export default function WorkspaceSettingsMenu() {
-  const pathname = usePathname();
-  const showMenu = pathname
-    ? ALLOWED_PREFIXES.some((prefix) => (prefix === "/" ? pathname === "/" : pathname.startsWith(prefix)))
-    : false;
-
-  if (!showMenu) return null;
-
   return <SettingsMenu />;
 }
