060df9cf0bd8c7d541ab920a1980f62ccda2f749
Alan Morris <morrisalan2020@gmail.com>
2025-11-30 23:18:55 -0700

    update

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -120,7 +120,7 @@ export default function RegisterPage() {
       {/* Background image + overlay */}
       <div className="pointer-events-none absolute inset-0 z-0 overflow-hidden">
         <img
-          src="/girl-picture.svg"
+          src="/Girl-picture.svg"
           alt="MergifyPDF signup background"
           className="h-full w-full object-cover"
         />
