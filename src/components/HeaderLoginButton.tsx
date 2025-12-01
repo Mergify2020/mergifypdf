6c50679b9740c89d2bfe32700025c62937be440f
Alan Morris <morrisalan2020@gmail.com>
2025-11-30 19:43:27 -0700

    update

--- src/components/HeaderLoginButton.tsx
+++ src/components/HeaderLoginButton.tsx
@@ -1,8 +1,15 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function HeaderLoginButton() {
+  const pathname = usePathname();
+
+  if (pathname === "/login") {
+    return null;
+  }
+
   return (
     <Link
       href="/login"
