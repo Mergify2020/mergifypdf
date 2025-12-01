0a95c4324e1862a761a573831d8649c15f952b09
Alan Morris <morrisalan2020@gmail.com>
2025-12-01 13:39:06 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 import UploadCta from "@/components/UploadCta";
@@ -58,6 +59,9 @@ function Sparkle({ className, gradientId }: { className?: string; gradientId: st
 export default async function Home() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
+    if (session.user.twoFactorEnabled && !session.user.twoFactorPassed) {
+      redirect("/2fa");
+    }
     return <ProjectsDashboard displayName={session.user.name ?? session.user.email ?? "Guest"} />;
   }
 
